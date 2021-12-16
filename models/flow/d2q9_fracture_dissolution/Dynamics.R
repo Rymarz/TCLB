9bd63fce0b3d2433254900d88fa28f9e6c7c2bfa
Tadeusz Rymarz <t.j.rymarz@gmail.com>
2021-12-16 19:06:01 +0100

    d2q5 MRT for solute with reaction

--- models/flow/d2q9_fracture_dissolution/Dynamics.R
+++ models/flow/d2q9_fracture_dissolution/Dynamics.R
@@ -14,10 +14,6 @@ AddDensity( name="c[1]", dx= 1, dy= 0, group="c")
 AddDensity( name="c[2]", dx= 0, dy= 1, group="c")
 AddDensity( name="c[3]", dx=-1, dy= 0, group="c")
 AddDensity( name="c[4]", dx= 0, dy=-1, group="c")
-AddDensity( name="c[5]", dx= 1, dy= 1, group="c")
-AddDensity( name="c[6]", dx=-1, dy= 1, group="c")
-AddDensity( name="c[7]", dx=-1, dy=-1, group="c")
-AddDensity( name="c[8]", dx= 1, dy=-1, group="c")
 
 AddDensity( name="h_Z", dx=0, dy=0, group="HZ")
 
@@ -41,7 +37,6 @@ AddSetting(name="Viscosity", RelaxationRate='1.0/(3*Viscosity + 0.5)', default=0
 AddSetting(name="VelocityX", default=0, comment='inlet/outlet/init velocity', zonal=T)
 AddSetting(name="VelocityY", default=0, comment='inlet/outlet/init velocity', zonal=T)
 AddSetting(name="Pressure", default=0, comment='inlet/outlet/init density', zonal=T)
-
 AddSetting(name="GravitationX")
 AddSetting(name="GravitationY")
 
@@ -51,19 +46,30 @@ AddSetting(name="S4", default="0", comment='MRT Sx')
 
 AddSetting(name="nubuffer", default=0.01, comment='Viscosity in the buffer layer (cumulant)')
 
+AddSetting(name="Omega_D", SC1="1-Omega_D", default=0, comment='one over concentration relaxation rate')
+AddSetting(name="D",  Omega_D="1.0/(3*D+0.5)", default=0.16666666, comment='Diffusivity') 
 AddSetting(name="Height", default=1, zonal=TRUE) 
 AddSetting(name="C_init", default=0, zonal=TRUE) 
-AddSetting(name="omega_D", comment='Relaxation rate') 
-AddSetting(name="D", omega_D='1.0/(3*D+0.5)', default=0.16666666, comment='Diffusivity') 
 AddSetting(name="C_inlet", default=0.0)
 AddSetting(name="C_outlet", default=0.0)
 AddSetting(name="C_saturation", default=1)
-
 AddSetting(name="k",default=1, comment='Reaction speed coefficient')
 AddSetting(name="C_solid", default=100, comment='Solid concentration')
 
+AddSetting(name="SC1", default="0", comment='MRT CSx')
+AddSetting(name="SC2", default="0", comment='MRT CSx')
+
+# AddSetting(name="Omega_D", default=0, comment='SRT concetration relaxation rate')
+# AddSetting(name="Omega_D_even", default=0, comment='TRT even concetration relaxation rate')
+# AddSetting(name="Omega_D_odd", Omega_D _even="(1.0 - 0.5*Omega_D_odd)/((MagicNumber_D-0.25)*Omega_D_odd + 0.5)", default=0, comment='TRT odd concetration relaxation rate')
+# AddSetting(name="MagicNumber_D", default=0.25, comment='TRT concetration magic number')
+
+# AddSetting(name="D",  Omega_D='1.0/(3*D+0.5)', default=0.16666666, comment='Diffusivity') 
+# AddSetting(name="D",  Omega_D_odd="1.0/(3*D+0.5)", default=0.16666666, comment='Diffusivity') 
+
+
 # Globals - table of global integrals that can be monitored and optimized
-AddGlobal(name="PressureLoss", comment='pressure loss', unit="1mPa")
+AddGlobal(name="PressureLoss", comment='pressure loss', unit="1mPa") 
 AddGlobal(name="OutletFlux", comment='pressure loss', unit="1m2/s")
 AddGlobal(name="InletFlux", comment='pressure loss', unit="1m2/s")
 
@@ -75,6 +81,10 @@ AddNodeType(name="NVelocity", group="BOUNDARY")
 AddNodeType(name="SVelocity", group="BOUNDARY")
 AddNodeType(name="WVelocity", group="BOUNDARY")
 AddNodeType(name="EVelocity", group="BOUNDARY")
+
+AddNodeType(name="WPressureWConcentration", group="BOUNDARY")
+AddNodeType(name="EPressureEConcentration", group="BOUNDARY")
+AddNodeType(name="WVelocityWConcentration", group="BOUNDARY")
 
 AddNodeType(name="NSymmetry", group="BOUNDARY")
 AddNodeType(name="SSymmetry", group="BOUNDARY")
